a9157807eb02bff62dd524ee559eaee4768d9675
Ilya Zhiltsov <izhiltsov@apple.com>
2021-03-16 23:35:15 -0700

    Added CLI

---
+++ src/cli.js
@@ -0,0 +1,11 @@
+'use strict'
+
+import readlineSync from 'readline-sync';
+
+const getName = () => {
+	const name = readlineSync.question('May I have your name? ');
+	return `Hello, ${name}!`;
+};
+
+
+export { getName };
