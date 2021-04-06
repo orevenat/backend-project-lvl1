6cf0ffe1a504a7065e3671653f31fbc6f4ab6daa
Ilya Zhiltsov <izhiltsov@apple.com>
2021-04-06 09:39:48 -0700

    Progrression and prime

--- src/games/prime.js
+++ src/games/prime.js
@@ -1,35 +1,20 @@
-import gamePlay from '../index.js';
+import gameEngine from '../index.js';
 import getRandomInt from '../utils.js';
 
-const gameQuestion = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+const gameRule = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
+const isPrime = (num) => {
+  for (let i = 2; i < num; i += 1) {
+    if (num % i === 0) return false;
+  }
+  return num > 1;
+};
 
+const gameData = () => {
+  const randomNumber = getRandomInt();
+  const answer = (isPrime(randomNumber)) ? 'yes' : 'no';
+  const question = String(randomNumber);
+  return [question, answer];
+};
 
-
-
-
-
-
-
-
-// const primeGame = () => {
-
-//   };
-
-//   const isPrime = (num) => {
-//     for (let i = 2; i < num; i += 1) {
-//       if (num % i === 0) return false;
-//     }
-//     return num > 1;
-//   };
-//   for (let i = 0; i < 3; i += 1) {
-//     const number = getRandomInt();
-//     const correctAnswer = (isPrime(number)) ? 'yes' : 'no';
-//     gameDataObj[i] = [number, correctAnswer];
-//   }
-
-//   const result = gamePlay(gameDataObj);
-//   return result;
-// };
-
-// export default primeGame;
+export default () => gameEngine(gameData, gameRule);
--- src/games/progression.js
+++ src/games/progression.js
@@ -1,61 +1,28 @@
-import gamePlay from '../index.js';
+import gameEngine from '../index.js';
 import getRandomInt from '../utils.js';
 
-const gameQuestion = 'What number is missing in the progression?';
+const gameRule = 'What number is missing in the progression?';
 
+const makeProgression = (length, step) => {
+  const progression = [];
+  let member = getRandomInt();
+  for (let i = 0; i < length; i += 1) {
+    progression.push(member);
+    member += step;
+  }
+  return progression;
+};
 
+const gameData = () => {
+  const lengthOfProgression = 10; // how many members in progression
+  const step = getRandomInt(10); // step of progression
 
+  const progression = makeProgression(lengthOfProgression, step);
+  const randomIndex = getRandomInt(progression.length - 1);
+  const answer = String(progression.splice(randomIndex, 1, '..')[0]);
+  const question = progression.join(' ');
 
+  return [question, answer];
+};
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const progressionGame = () => {
-//   const gameDataObj = {
-//     gameQuestion: 'What number is missing in the progression?',
-//   };
-
-//   const makeProgression = (length, step) => {
-//     const progression = [];
-//     let member = getRandomInt();
-//     for (let i = 0; i < length; i += 1) {
-//       progression.push(member);
-//       member += step;
-//     }
-//     return progression;
-//   };
-
-//   const lengthOfProgression = 10; // how many members in progression
-//   const step = getRandomInt(10); // step of progression
-
-//   for (let i = 0; i < 3; i += 1) {
-//     const progression = makeProgression(lengthOfProgression, step);
-//     const randomIndex = getRandomInt(progression.length - 1);
-//     const correctAnswer = String(progression.splice(randomIndex, 1, '..')[0]);
-//     const expression = progression.join(' ');
-//     gameDataObj[i] = [expression, correctAnswer];
-//   }
-
-//   const result = gamePlay(gameDataObj);
-//   return result;
-// };
-
-// export default progressionGame;
+export default () => gameEngine(gameData, gameRule);
