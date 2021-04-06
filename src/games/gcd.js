7255e7843430aa683896c5b2a4f13219f5d8ccc4
Ilya Zhiltsov <izhiltsov@apple.com>
2021-04-05 22:19:44 -0700

    gcd implemented

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -1,37 +1,22 @@
-import gamePlay from '../index.js';
+import gameEngine from '../index.js';
 import getRandomInt from '../utils.js';
 
-const gameQuestion = 'Find the greatest common divisor of given numbers.';
+const gameRule = 'Find the greatest common divisor of given numbers.';
 
+const gameData = () => {
+  const gcd = (a, b) => {
+    if (b === 0) {
+      return a;
+    }
+    return gcd(b, a % b);
+  };
+  const maxRandomNumber = 20;
+  const num1 = getRandomInt(maxRandomNumber);
+  const num2 = getRandomInt(maxRandomNumber);
+  const question = `${num1} ${num2}`;
+  const answer = String(gcd(num1, num2));
 
+  return [question, answer];
+};
 
-
-
-
-
-
-// const gcdGame = () => {
-//   const gameDataObj = {};
-//   gameDataObj.gameQuestion = 'Find the greatest common divisor of given numbers.';
-
-//   const gameLevel = 20;
-//   const gcd = (a, b) => {
-//     if (b === 0) {
-//       return a;
-//     }
-//     return gcd(b, a % b);
-//   };
-
-//   for (let i = 0; i < 3; i += 1) {
-//     const num1 = getRandomInt(gameLevel);
-//     const num2 = getRandomInt(gameLevel);
-//     const numPair = `${num1} ${num2}`;
-//     const correctAnswer = String(gcd(num1, num2));
-//     gameDataObj[i] = [numPair, correctAnswer];
-//   }
-
-//   const result = gamePlay(gameDataObj);
-//   return result;
-// };
-
-// export default gcdGame;
+export default () => gameEngine(gameData, gameRule);
